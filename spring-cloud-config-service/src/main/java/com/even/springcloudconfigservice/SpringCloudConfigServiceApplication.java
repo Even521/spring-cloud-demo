5251f78c0f4bf384a73351057555ae6df3e6e122
even <even366>
2019-04-19 10:09:20 +0800

    配置服务

--- spring-cloud-config-service/src/main/java/com/even/springcloudconfigservice/SpringCloudConfigServiceApplication.java
+++ spring-cloud-config-service/src/main/java/com/even/springcloudconfigservice/SpringCloudConfigServiceApplication.java
@@ -2,9 +2,11 @@ package com.even.springcloudconfigservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.config.server.EnableConfigServer;
 
 
 @SpringBootApplication
+@EnableConfigServer
 public class SpringCloudConfigServiceApplication {
 
     public static void main(String[] args) {
