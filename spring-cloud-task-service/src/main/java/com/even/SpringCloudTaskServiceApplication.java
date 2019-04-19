295be4bf0bc66e78bc57b193e3493b2e08139c57
even <even366>
2019-04-19 17:14:15 +0800

    admin user service

---
+++ spring-cloud-task-service/src/main/java/com/even/SpringCloudTaskServiceApplication.java
@@ -0,0 +1,31 @@
+package com.even;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.task.configuration.EnableTask;
+import org.springframework.context.annotation.Bean;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@SpringBootApplication
+@EnableTask
+public class SpringCloudTaskServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudTaskServiceApplication.class, args);
+    }
+    @Bean
+    public CommandLineRunner commandLineRunner() {
+        return new TestCommandLineRunner();
+    }
+
+    public static class TestCommandLineRunner implements CommandLineRunner {
+        @Override
+        public void run(String... strings) throws Exception {
+            System.out.println("this is a Test about spring cloud task.");
+        }
+    }
+
+}
