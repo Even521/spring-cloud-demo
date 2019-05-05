d52e79fca5760904d699d1f32127357e0a9b1174
even <even366>
2019-05-05 17:10:04 +0800

    添加限流

--- spring-cloud-gateway-service/src/main/java/com/even/SpringCloudGatewayServiceApplication.java
+++ spring-cloud-gateway-service/src/main/java/com/even/SpringCloudGatewayServiceApplication.java
@@ -1,7 +1,9 @@
 package com.even;
 
+import com.even.currentlimiting.HostAddrKeyResolver;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class SpringCloudGatewayServiceApplication {
@@ -9,5 +11,8 @@ public class SpringCloudGatewayServiceApplication {
     public static void main(String[] args) {
         SpringApplication.run(SpringCloudGatewayServiceApplication.class, args);
     }
-
+    @Bean
+    public HostAddrKeyResolver hostAddrKeyResolver() {
+        return new HostAddrKeyResolver();
+    }
 }
---
+++ spring-cloud-gateway-service/src/main/java/com/even/currentlimiting/HostAddrKeyResolver.java
@@ -0,0 +1,19 @@
+package com.even.currentlimiting;
+
+import org.springframework.cloud.gateway.filter.ratelimit.KeyResolver;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+/**
+ * @author even
+ * @date 2019/5/5 16:56
+ * @describe
+ */
+public class HostAddrKeyResolver implements KeyResolver {
+    @Override
+    public Mono<String> resolve(ServerWebExchange exchange) {
+        return Mono.just(exchange.getRequest().getRemoteAddress().getAddress().getHostAddress());
+    }
+
+}
